522eee116575edec85490f3e724a5fe23cf80a88
yelim <redheadannielim@gmail.com>
2024-04-09 19:23:03 +0900

    chore: antd 적용 테스트

--- src/Pages/CreateGroupPage/index.js
+++ src/Pages/CreateGroupPage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 // import styled from 'styled-components'
 
 const CreateGroup = () => {
-    return <div>CreateGroup페이지</div>
+    return <div></div>
 }
 
 export default CreateGroup
--- src/Pages/ShowGroupList/index.js
+++ src/Pages/ShowGroupList/index.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Card, Col, Row } from 'antd'
 
 const ShowGroupList = () => {
-    return <div>ShowGroupList 페이지</div>
+    return (
+        <Row gutter={16}>
+            <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                    Card content
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                    Card content
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                    Card content
+                </Card>
+            </Col>
+        </Row>
+    )
 }
 
 export default ShowGroupList
