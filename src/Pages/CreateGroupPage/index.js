f84d7ad717c315593a6a8058edc870395a261007
조준호 <junho@jojunhoui-MacBookAir.local>
2024-04-14 02:39:21 +0900

    docs: import styled-component 삭제

--- src/Pages/CreateGroupPage/index.js
+++ src/Pages/CreateGroupPage/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CreateForm from '../../Components/Forms/CreateForm'
-// import styled from 'styled-components'
+//import CreateForm from '../../Components/Forms/CreateForm'
 
 const CreateGroup = () => {
     return (
