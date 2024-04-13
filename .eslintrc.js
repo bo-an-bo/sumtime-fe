f4c761499a1e2bb8618e22bd49df0ca8b2a075bf
조준호 <junho@jojunhoui-MacBookAir.local>
2024-04-14 02:30:37 +0900

    ci: 설정 파일 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,29 @@
 module.exports = {
     env: {
+        browser: true,
+        es2021: true,
         node: true,
+        jest: true,
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    plugins: ['react'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: ['react', 'react-hooks', 'jsx-a11y'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+        react: { version: 'detect' },
+    },
 }
