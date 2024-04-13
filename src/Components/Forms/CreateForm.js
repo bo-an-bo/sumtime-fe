d62323a826b99f50e0846929a04bba3445afcab8
조준호 <junho@jojunhoui-MacBookAir.local>
2024-04-14 02:35:11 +0900

    feat: 헤더 영역 추가

--- src/Components/Forms/CreateForm.js
+++ src/Components/Forms/CreateForm.js
@@ -4,6 +4,7 @@ import { Form, Input, Button, Upload } from 'antd'
 import styled from 'styled-components'
 
 // 폼 전송 성공 시 실행되는 함수
+/* Create 구현 필요 */
 const onFinish = async (values) => {
     try {
         const formData = new FormData()
@@ -13,7 +14,7 @@ const onFinish = async (values) => {
             formData.append('file', values.fileList[0].originFileObj)
         }
 
-        const response = await fetch('서버 URL', {
+        const response = await fetch('http://localhost:3001/groups', {
             method: 'POST',
             body: formData,
         })
@@ -44,33 +45,52 @@ const normFile = (e) => {
 
 // 전체 폼을 감싸는 스타일드 컴포넌트
 const StyledForm = styled(Form)`
-    margin: 20px;
+    margin: 30px;
 `
 // 폼을 감싸는 스타일드 컴포넌트
 const StyledFormWrapper = styled.div`
-    margin-bottom: 10px;
     padding: 10px;
     border-radius: 10px;
-    background-color: #f0f2f5;
-    max-width: 400px;
+    background-color: rgba(0, 62.67, 151.94, 0.08);
+    max-width: 800px;
     margin: 10px auto;
 `
+
 // 타이틀을 감싸는 스타일드 컴포넌트
 const Title = styled.h1`
-    margin: 10px auto;
-    text-align: center;
+    margin: 10px 40px;
+    text-align: left;
+    font-size: 32px;
+    font-family: 'KoPubWorld Dotum';
+    font-weight: 700;
+    word-wrap: break-word;
 `
 // 폼 아이템을 감싸는 스타일드 컴포넌트
 const StyledFormItems = styled(Form.Item)`
+    .ant-form-item-label {
+        text-align: left;
+        margin-left: 40px;
+        font-size: 24px;
+        font-family: 'KoPubWorld Dotum';
+        font-weight: 700;
+        word-wrap: break-word;
+    }
+    .ant-input {
+        text-align: left;
+        font-size: 14px;
+        margin-top: 6px;
+    }
     .ant-btn {
-        font-size: 16px;
+        height: 50px;
+        width: 100px;
+        font-size: 24px;
     }
 `
 const CreateForm = () => (
     <StyledForm
         name="basic"
         labelCol={{
-            span: 6,
+            span: 3,
         }}
         wrapperCol={{
             span: 16,
@@ -84,16 +104,7 @@ const CreateForm = () => (
     >
         <StyledFormWrapper>
             <Title>모임 기본 정보 등록</Title>
-            <StyledFormItems
-                label="모임 이름"
-                name="groupname"
-                rules={[
-                    {
-                        required: true,
-                        message: '모임 이름을 입력하세요!',
-                    },
-                ]}
-            >
+            <StyledFormItems label="모임 이름" name="groupname">
                 <Input placeholder="ex) 한사랑산악회, 숭실대 IT대 학생회" />
             </StyledFormItems>
             <StyledFormItems label="모임 설명" name="groupdescription">
@@ -103,7 +114,7 @@ const CreateForm = () => (
         <StyledFormWrapper>
             <Title>모임 회원 등록</Title>
             <StyledFormItems label="파일 업로드" valuePropName="fileList" getValueFromEvent={normFile}>
-                <Upload action="/upload.do" listType="picture-card" maxCount={1}>
+                <Upload action="/upload.do" accept=".csv, .xlsx" listType="picture-card" maxCount={1}>
                     <button
                         style={{
                             border: 0,
