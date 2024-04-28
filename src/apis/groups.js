a2fccd89680a323458065dbb4fda93018fe0f5d9
조준호 <junho@jojunhoui-MacBookAir.local>
2024-04-28 21:07:39 +0900

    feat: execel upload

--- src/apis/groups.js
+++ src/apis/groups.js
@@ -1,6 +1,12 @@
 import api from './index'
+import { formApi } from './index'
 
 export const getGroups = async () => {
     const response = await api.get('/group')
+    return response.data
+}
+
+export const createGroup = async (groupInfo) => {
+    const response = await formApi.post('/group', groupInfo)
     return response.data
 }
