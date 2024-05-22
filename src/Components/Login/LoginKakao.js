0dfb500be2f630644e765e44cde9b1da1deb2712
ssoxong <ssoxong@naver.com>
2024-05-22 21:48:27 +0900

    feat: sdk 사용해서 로그인

--- src/Components/Login/LoginKakao.js
+++ src/Components/Login/LoginKakao.js
@@ -1,40 +1,83 @@
-import React from 'react'
-import KakaoLogin from 'react-kakao-login'
-import { loginKakao } from '../../apis/auth'
+import React, { useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const LoginKakao = ({ onLogin }) => {
-    const kakaoClientId = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY
+    const [user, setUser] = useState(null)
+    const [isLogin, setIsLogin] = useState(false)
+    const Kakao = window.Kakao || {}
+    if (onLogin) onLogin()
+    // Ensure Kakao is initialized
+    const initKakao = useCallback(() => {
+        if (Kakao && !Kakao.isInitialized()) {
+            Kakao.init(process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY)
+            console.log(Kakao.isInitialized())
+        }
+    }, [])
 
-    const kakaoOnSuccess = async (data) => {
-        try {
-            const idToken = data.response.access_token
-            const loginResponse = await loginKakao(idToken)
-            console.log('loginResponse', loginResponse)
+    const kakaoLogin = async () => {
+        if (Kakao && Kakao.Auth) {
+            await Kakao.Auth.login({
+                success(res) {
+                    console.log(res)
+                    Kakao.Auth.setAccessToken(res.access_token)
+                    console.log('카카오 로그인 성공')
 
-            // 액세스 토큰을 로컬 스토리지에 저장
-            localStorage.setItem('access_token', idToken)
-            localStorage.setItem('nickname', loginResponse.data.properties.nickname)
-            if (onLogin) onLogin()
-        } catch (error) {
-            console.error('Login failed:', error)
+                    Kakao.API.request({
+                        url: '/v2/user/me',
+                        success(res) {
+                            console.log('카카오 인가 요청 성공', res)
+                            setIsLogin(true)
+                            const kakaoAccount = res.kakao_account
+                            localStorage.setItem('email', kakaoAccount.email)
+                            localStorage.setItem('profileImg', kakaoAccount.profile.profile_image_url)
+                            localStorage.setItem('nickname', kakaoAccount.profile.nickname)
+                        },
+                        fail(error) {
+                            console.log(error)
+                        },
+                    })
+                },
+                fail(error) {
+                    console.log(error)
+                },
+            })
         }
     }
 
-    const kakaoOnFailure = (error) => {
-        console.log('Login failed:', error)
-    }
+    useEffect(() => {
+        initKakao()
+    }, [])
+
+    useEffect(() => {
+        if (Kakao && Kakao.Auth) {
+            setIsLogin(!!Kakao.Auth.getAccessToken())
+        }
+    }, [])
+
+    useEffect(() => {
+        if (isLogin) {
+            setUser({
+                email: localStorage.getItem('email'),
+                profileImg: localStorage.getItem('profileImg'),
+                nickname: localStorage.getItem('nickname'),
+            })
+        }
+    }, [isLogin])
 
     return (
         <>
-            <KakaoLogin
-                token={kakaoClientId}
-                onSuccess={kakaoOnSuccess}
-                onFail={kakaoOnFailure}
-                render={({ onClick }) => (
-                    <KakaoButton onClick={onClick} />
-                )}
-            />
+            <KakaoButton onClick={kakaoLogin} />
+            {user && (
+                <div>
+                    <h2>카카오 로그인 성공!</h2>
+                    <h3>카카오 프로필 사진</h3>
+                    <img src={user.profileImg} alt="" />
+                    <h3>카카오 닉네임</h3>
+                    <h4>{user.nickname}</h4>
+                    <h3>카카오 이메일</h3>
+                    <h4>{user.email}</h4>
+                </div>
+            )}
         </>
     )
 }
@@ -49,4 +92,5 @@ const KakaoButton = styled.button`
         opacity: 0.9;
     }
 `
+
 export default LoginKakao
