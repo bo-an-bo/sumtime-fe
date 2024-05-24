84a90dc442484da35a188c1a4bdfd0f9d6bfa489
ssoxong <ssoxong@naver.com>
2024-05-25 01:15:11 +0900

    feat: 엔드포인트 변경

--- src/apis/event.js
+++ src/apis/event.js
@@ -1,5 +1,4 @@
 import api from './index'
-import axios from 'axios'
 
 export const postEvent = async (groupId, eventInfo) => {
     const response = await api.post(`group/${groupId}/event`, eventInfo)
@@ -12,7 +11,7 @@ export const getEvent = async (groupId) => {
 }
 
 export const getEventDetail = async (groupId, eventId) => {
-    const response = await axios.get(`http://localhost:5354/group/${groupId}/event/${eventId}`)
+    const response = await api.get(`group/${groupId}/event/${eventId}`)
     return response.data
 }
 
