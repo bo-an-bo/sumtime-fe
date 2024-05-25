fd3da320ed19a342ce804e7e1cd5c349e434f7e9
yelim <redheadannielim@gmail.com>
2024-05-25 09:22:36 +0900

    error resolving...

--- src/Pages/ShowResult/index.js
+++ src/Pages/ShowResult/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import EventChart from '../../Components/Chart/EventChart'
-import EventChart from '../../Components/Chart/EventChart'
 
 const ShowResult = () => {
     return (
