9c70bd8c59ff1136f74175f609ba57016842ee4a
ssoxong <ssoxong@naver.com>
2024-05-28 02:46:22 +0900

    feat: 로그인 회원탈퇴 처리

--- src/Components/Login/SignOutServer.js
+++ src/Components/Login/SignOutServer.js
@@ -2,22 +2,26 @@ import { Button } from 'antd'
 import React from 'react'
 import { useAuth } from '../../context/AuthContext'
 import { signOutServer } from '../../apis/auth'
+import { useNavigate } from 'react-router-dom'
 
 const SignOutServer = () => {
     const { setUser } = useAuth()
+    const navigate = useNavigate()
 
     const serverSignout = async () => {
         try {
             setUser(null)
             localStorage.clear()
             await signOutServer()
-            location.reload()
         } catch (error) {
             console.error('Server signout error:', error.response ? error.response.data : error.message)
         }
+        navigate('/')
+        location.reload()
     }
 
-    return <Button onClick={serverSignout}>로그아웃 및 회원 탈퇴</Button>
+    return <Button type="link" onClick={serverSignout}
+                   style={{ padding: '0', height: 'auto', lineHeight: 'inherit' }}>회원 탈퇴</Button>
 }
 
 export default SignOutServer
