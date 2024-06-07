af525a186caf79e80529538a26460671856db527
조준호 <junho@jojunhoui-MacBookAir.local>
2024-06-07 17:18:05 +0900

    design: chart layout in mobile

--- src/Pages/ShowResult/index.js
+++ src/Pages/ShowResult/index.js
@@ -98,6 +98,9 @@ const Wrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 100%;
-    height: 100%;
+
+    @media (max-width: 768px) {
+        width: 100%;
+        height: 100%;
+    }
 `
