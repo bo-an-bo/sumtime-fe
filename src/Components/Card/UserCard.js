f5d757ee4740f4f66924022aebc0ec5689d23ad3
조준호 <junho@jojunhoui-MacBookAir.local>
2024-06-09 02:19:35 +0900

    design: width unit change

--- src/Components/Card/UserCard.js
+++ src/Components/Card/UserCard.js
@@ -30,7 +30,7 @@ const StyledCard = styled.div`
     border-radius: 8px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     text-align: center;
-    width: 300px;
+    width: 80vw;
 `
 
 const UserName = styled.h2`
